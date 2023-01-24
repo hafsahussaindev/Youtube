2e0a472a18e20f8a3a923627e0615181418984b8
Zain <zainjaved965@gmail.com>
2023-01-24 12:49:52 +0500

    sidebar working properly

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let openMenu = document.getElementById("open-menu")
+let toggleMenu = document.querySelector("#show")
+let sideMenu =  document.querySelector(".nav-menu")
+let main = document.querySelector(".main")
+let navBar = document.querySelector(".nav")
+
+function showMenu() {
+    sideMenu.classList.add("active")
+    main.classList.add("side-effect")
+}
+
+function hideMenu(){
+    sideMenu.classList.remove("active")
+    main.classList.remove("side-effect")
+
+}
